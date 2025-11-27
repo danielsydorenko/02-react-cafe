65ac6ac91076f98854bdb4646fdce7ccc37cd4aa
danielsydorenko <danielsydorenko1@gmail.com>
2025-11-27 15:34:06 +0100

    Initial commit

---
+++ src/task-1.ts
@@ -0,0 +1,7 @@
+const Uname: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${Uname}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
