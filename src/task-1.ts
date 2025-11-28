33242692111bdbebb80bb89c88fc7dcf36ec4132
danielsydorenko <danielsydorenko1@gmail.com>
2025-11-28 18:25:22 +0100

    homework 2

--- src/task-1.ts
+++
@@ -1,7 +0,0 @@
-const Uname: string = "Alice";
-const age: number = 30;
-const isOnline: boolean = true;
-
-console.log(`Name: ${Uname}`);
-console.log(`Age: ${age}`);
-console.log(`Online: ${isOnline}`);
