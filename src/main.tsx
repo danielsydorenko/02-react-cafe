6a0f3124eed541a89d852e0cb57b0c05443e2bdf
danielsydorenko <danielsydorenko1@gmail.com>
2025-12-01 00:49:19 +0100

    homework 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize/modern-normalize.css';
+import App from './components/App/App';
 import './index.css';
-import App from './components/App/App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
